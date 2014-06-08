b095b34a2738b46aa715c19b76f1358d7a1ab93c
Michael Yang <mike@captain-refactor.co.za>
2014-06-08 15:26:50 +0200

    adjusted plot4 out put and add plot4.png

--- plot4.R
+++ plot4.R
@@ -7,16 +7,16 @@ csv$DateTime <- strptime(paste(csv$Date, csv$Time), "%d/%m/%Y %H:%M:%S")
 s <- subset(csv, DateTime >= as.POSIXlt('2007-02-01') & DateTime < as.POSIXlt('2007-02-03'))
 
 png(filename='plot4.png')
-par(mfcol=c(2,2))
+par(mfcol=c(2,2), cex.lab=0.85, cex.axis=0.85)
 
 # top left:
-plot(s$DateTime, s$Global_active_power, type='l', xlab='', ylab='Global Active Power (kilowatts)')
+plot(s$DateTime, s$Global_active_power, type='l', xlab='', ylab='Global Active Power')
 
 # bottom left:
 with(s, plot(DateTime, Sub_metering_1, type='l', xlab='', ylab='Energy sub metering'))
 with(s, lines(DateTime, Sub_metering_2, col='red'))
 with(s, lines(DateTime, Sub_metering_3, col='blue'))
-legend("topright", lty=1, col = c("black", "red", "blue"),
+legend("topright", lty=1, col = c("black", "red", "blue"), cex=0.7, bty="n",
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 # top right:
