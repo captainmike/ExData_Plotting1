aefec386cb32a56606f1080ed009d1244dc6e954
Michael Yang <mike@captain-refactor.co.za>
2014-06-08 12:46:30 +0200

    add plot1.R and plot1.png

--- plot1.R
+++ plot1.R
@@ -1,3 +1,12 @@
-csv <- read.csv("./data/household_power_consumption.txt", sep = ";", na.strings = "?")
-csv$DateTime <- strptime(paste(csv$Date, csv$Time), "%d/%m/%Y %H:%M:%S")
-s <- subset(csv, DateTime >= as.POSIXlt('2007-02-01') & DateTime < as.POSIXlt('2007-02-03'))
+# The function assumes data had been downloaded from:
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# and unzipped to 'data' folder in the working directory.
+plot1 <- function() {
+  csv <- read.csv("./data/household_power_consumption.txt", sep = ";", na.strings = "?")
+  csv$DateTime <- strptime(paste(csv$Date, csv$Time), "%d/%m/%Y %H:%M:%S")
+  s <- subset(csv, DateTime >= as.POSIXlt('2007-02-01') & DateTime < as.POSIXlt('2007-02-03'))
+  
+  png(filename='plot1.png')
+  hist(s$Global_active_power, col='red', main='Global Active Power', xlab = 'Global Active Power (kilowatts)')
+  dev.off()
+}
